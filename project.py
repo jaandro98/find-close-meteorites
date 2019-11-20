7d21a6cbabfb191778e41aadcd77b5b3dbece6ed
Benito Sanchez <a.a.benito.sanchez@accenture.com>
2019-11-20 12:01:00 +0100

    Modifying script

--- project.py
+++ project.py
@@ -1,7 +1,15 @@
 import boto3
+import click
 
 session = boto3.Session(profile_name='Jandro')
 ec2 = session.resource('ec2',region_name='eu-west-1')
 
-for i in ec2.instances.all():
-    print(i)
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print(i.id, i.instance_type, i.state['Name'])
+
+
+if __name__ == "__main__":
+    list_instances()
